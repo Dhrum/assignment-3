f34e6876c627a5f2f314461f1acdd6bf8676fcc5
Dhrum <rahim.c4d@gmail.com>
2024-09-23 19:19:21 +0600

    Added carousel functionality and styling changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';  // Import ToastContainer
+import 'react-toastify/dist/ReactToastify.css';  // Import toast styles
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import FAQ from './components/FAQ';
+import BookDetails from './components/BookDetails';
+import NotFound from './components/NotFound';
+import Footer from './components/Footer';
+import './App.css';  // Import global CSS styles
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/book/:id" element={<BookDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ToastContainer />  {/* Add ToastContainer globally */}
+      <Footer/>
+    </Router>
+    
   );
 }
 
